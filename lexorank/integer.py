637ff376f1f93aa54dfb966d6c74b22a6ed01f37
Hyeonki Hong <hhk7734@gmail.com>
2023-08-23 20:55:38 +0900

    feat: add Integer

---
+++ lexorank/integer.py
@@ -0,0 +1,128 @@
+from enum import IntEnum
+
+from typing_extensions import Self
+
+from lexorank.base import Base, Base36
+
+
+class Sign(IntEnum):
+    POSITIVE = 1
+    NEGATIVE = -1
+
+    def __neg__(self) -> Self:
+        return self.__class__(-self.value)
+
+    def __mul__(self, other: Self) -> Self:  # type: ignore[override]
+        return self.__class__(self.value * other.value)
+
+
+class Integer:
+    def __init__(
+        self, reversed_digits: list[int], sign: Sign = Sign.POSITIVE, base: Base = Base36
+    ) -> None:
+        self._rdigits = self._rstrip(reversed_digits)
+        self._sign = sign
+        self._base = base
+
+    @property
+    def digits(self) -> list[int]:
+        return list(reversed(self._rdigits))
+
+    @property
+    def base(self) -> int:
+        return self._base.base()
+
+    @staticmethod
+    def from_base(value: str, base: Base = Base36) -> "Integer":
+        sign = Sign.POSITIVE if value[0] != "-" else Sign.NEGATIVE
+        if value[0] in ("+", "-"):
+            value = value[1:]
+
+        digits = [base.to_base10(digit) for digit in reversed(value)]
+        return Integer(digits, sign, base)
+
+    @staticmethod
+    def from_base10(value: int, base: Base = Base36) -> "Integer":
+        sign = Sign.POSITIVE if value >= 0 else Sign.NEGATIVE
+        value = abs(value)
+
+        digits = []
+        while value > 0:
+            digits.append(value % base.base())
+            value //= base.base()
+
+        return Integer(digits, sign, base)
+
+    def __neg__(self) -> Self:
+        return self.__class__(self._rdigits.copy(), -self._sign, self._base)
+
+    def __add__(self, other: Self) -> Self:
+        if self._sign != other._sign:
+            return self - (-other)
+
+        rdigits = []
+        carry = 0
+
+        for i in range(max(len(self), len(other))):
+            a = self._rdigits[i] if i < len(self) else 0
+            b = other._rdigits[i] if i < len(other) else 0
+
+            result = a + b + carry
+            carry = result // self._base.base()
+            rdigits.append(result % self._base.base())
+
+        if carry:
+            rdigits.append(carry)
+
+        return self.__class__(rdigits, self._sign, self._base)
+
+    def __sub__(self, other: Self) -> Self:
+        raise NotImplementedError
+
+    def __mul__(self, other: Self) -> Self:
+        rdigits = [0] * (len(self) + len(other))
+
+        for i in range(len(self)):
+            for j in range(len(other)):
+                index = i + j
+                ret = rdigits[index] + self._rdigits[i] * other._rdigits[j]
+                rdigits[index] = ret % self._base.base()
+                rdigits[index + 1] += ret // self._base.base()
+
+        return self.__class__(rdigits, self._sign * other._sign, self._base)
+
+    def __lshift__(self, other: int) -> Self:
+        if other < 0:
+            raise ValueError("shift count must be non-negative")
+        rdigits = [0] * other + self._rdigits
+        return self.__class__(rdigits, self._sign, self._base)
+
+    def __rshift__(self, other: int) -> Self:
+        if other < 0:
+            raise ValueError("shift count must be non-negative")
+        return self.__class__(self._rdigits[other:], self._sign, self._base)
+
+    def to_base10(self) -> int:
+        value = 0
+        for i, digit in enumerate(self._rdigits):
+            value += digit * self._base.base() ** i
+        return self._sign.value * value
+
+    def __str__(self) -> str:
+        if len(self) == 0:
+            return "0"
+
+        num = "".join([self._base.from_base10(digit) for digit in reversed(self._rdigits)])
+        return f"{'-' if self._sign == Sign.NEGATIVE else ''}{num}"
+
+    def __len__(self) -> int:
+        return len(self._rdigits)
+
+    @staticmethod
+    def _rstrip(digits: list[int]) -> list[int]:
+        index = len(digits)
+        for i in reversed(range(index)):
+            if digits[i] != 0:
+                index = i + 1
+                break
+        return digits[:index]
---
+++ lexorank/integer_test.py
@@ -0,0 +1,97 @@
+from lexorank.base import Base10, Base36, Base64
+from lexorank.integer import Integer
+
+
+def test_from_base():
+    cases = [
+        {"in": ("123", Base10), "want": [1, 2, 3]},
+        {"in": ("ri", Base36), "want": [27, 18]},
+        {"in": ("r^i", Base64), "want": [55, 36, 46]},
+    ]
+
+    for c in cases:
+        got = Integer.from_base(*c["in"]).digits
+        assert got == c["want"]
+
+
+def test_from_base10():
+    cases = [
+        {"in": (123, Base10), "want": [1, 2, 3]},
+        {"in": (3 * 36 + 4, Base36), "want": [3, 4]},
+        {"in": (20 * 64**2 + 1, Base64), "want": [20, 0, 1]},
+    ]
+
+    for c in cases:
+        got = Integer.from_base10(*c["in"]).digits
+        assert got == c["want"]
+
+
+def test_add():
+    cases = [
+        {"in": (123, 356), "want": 479},
+        {
+            "in": (-11230124540123123124, -12315810924310958102938109238190284),
+            "want": -12315810924310969333062649361313408,
+        },
+    ]
+
+    for c in cases:
+        a = Integer.from_base10(c["in"][0])
+        b = Integer.from_base10(c["in"][1])
+        got = (a + b).to_base10()
+        assert got == c["want"]
+
+
+def test_mul():
+    cases = [
+        {"in": (123, 356), "want": 43788},
+        {"in": (-157564, 123), "want": -19380372},
+        {
+            "in": (-11230124540123123124, -12315810924310958102938109238190284),
+            "want": 138308090492620934298311599366068267126028607872527216,
+        },
+        {"in": (0, -123), "want": 0},
+    ]
+
+    for c in cases:
+        a = Integer.from_base10(c["in"][0])
+        b = Integer.from_base10(c["in"][1])
+        got = (a * b).to_base10()
+        assert got == c["want"]
+
+
+def test_lshift():
+    cases = [
+        {"in": (123, 1), "want": 123 * 36},
+        {"in": (123, 123), "want": 123 * 36**123},
+    ]
+
+    for c in cases:
+        a = Integer.from_base10(c["in"][0])
+        got = (a << c["in"][1]).to_base10()
+        assert got == c["want"]
+
+
+def test_rshift():
+    cases = [
+        {"in": (123, 1), "want": 123 // 36},
+        {"in": (123, 11), "want": 0},
+    ]
+
+    for c in cases:
+        a = Integer.from_base10(c["in"][0])
+        got = (a >> c["in"][1]).to_base10()
+        assert got == c["want"]
+
+
+def test_str():
+    cases = [
+        {"in": (123, Base10), "want": "123"},
+        {"in": (123123541345346245634523463456456, Base36), "want": "97l0vq9nx1bzufjaqp4ug"},
+        {"in": (123, Base64), "want": "1v"},
+        {"in": (-123, Base64), "want": "-1v"},
+    ]
+
+    for c in cases:
+        got = str(Integer.from_base10(*c["in"]))
+        assert got == c["want"]
